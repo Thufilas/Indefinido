091f3e842edf3058b4cd6b51655d01d650d63576
Thufilas <Jvictormagalhaes@gmail.com>
2020-02-20 21:46:20 -0300

    commit

---
+++ Indefinito/Entidade.dominio/Entidade/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidade.dominio.Entidade
+{
+    class Usuario
+    {
+        public int pessoaID { get; set; }
+        public string Nome { get; set; }
+
+    }
+}
---
+++ Indefinito/Usuario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+		public int pessoaID { get; set; }
+		public string Nome { get; set; }
+	}
+}
