63bd1abba7fa359490cae3129e9e0de76ecc942b
Pops <vinib.almeida@gmail.com>
2020-05-05 00:44:04 -0300

    init

---
+++ Indefinito/RMJ.Comum/NotificationPattern/NotificationResultExcepetion.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RMJ.Comum.NotificationPattern
+{
+    class NotificationResultExcepetion
+    {
+    }
+}
---
+++ Indefinito/RMJ.Comum/Utils/MatematicaUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RMJ.Comum
+{
+    class MatematicaUtil
+    {
+        public static decimal Divisao(decimal a, decimal b)
+        {
+            if (b == 0)
+                return 0;
+
+            return a / b;
+        }
+    }
+}
