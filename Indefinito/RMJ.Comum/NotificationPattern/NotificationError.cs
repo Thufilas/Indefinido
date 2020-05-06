7201b78bf4f20783b783611e59919e6f3f42567e
Pops <vinib.almeida@gmail.com>
2020-05-05 21:18:32 -0300

    init

--- Indefinito/RMJ.Comum/NotificationPattern/NotificationError.cs
+++ Indefinito/RMJ.Comum/NotificationPattern/NotificationError.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace RMJ.Comum.NotificationPattern
 {
-    class NotificationError
+    public enum NotificationErrorType
     {
+        UNKNOWN = 0,
+        BUSINESS_RULES = 1,
+        DATABASE = 2,
+        CONNECTION = 3,
+        USER = 4
+    }
+    public class NotificationError
+    {
+        public string Message { get; }
+        public NotificationErrorType Type { get; }
+
+        public NotificationError(string message, NotificationErrorType type = NotificationErrorType.UNKNOWN)
+        {
+            Message = message;
+            Type = type; 
+        }
     }
 }
--- Indefinito/RMJ.Comum/NotificationPattern/NotificationResultExcepetion.cs
+++ Indefinito/RMJ.Comum/NotificationPattern/NotificationResultExcepetion.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace RMJ.Comum.NotificationPattern
 {
-    class NotificationResultExcepetion
+    public class NotificationResultExcepetion
     {
+        private readonly Exception ex;
+
+        public NotificationResultExcepetion(Exception ex)
+        {
+            this.ex = ex;
+        }
+
+        public string PreMessage { get; set; }
+
+        private string MessageError
+        {
+            get
+            {
+                string str = "";
+                if (!string.IsNullOrEmpty(PreMessage))
+                    str = PreMessage + " : ";
+
+                str += ex.InnerException != null ? ex.InnerException.Message : ex.Message;
+
+                return str;
+            }
+        }
+
+        public NotificationResult Result => new NotificationResult().Add(new NotificationResult().Add(MessageError));
     }
 }
+
